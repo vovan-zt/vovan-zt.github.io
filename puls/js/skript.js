681e4be8676d5f82d779f4b23a7c45f9e4f87dff
vovan-zt <75982068+vovan-zt@users.noreply.github.com>
2022-04-19 17:38:18 +0300

    Add files via upload

---
+++ puls/js/skript.js
@@ -0,0 +1,115 @@
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav:false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev'); 
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next'); 
+});
+
+
+(function($) {
+  $(function() {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+  $('.catalog-item__link')
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
+
+
+//modal
+  $('[data-modal=consultation]').on('click', function(){ 
+    $('.overlay, #consultation').fadeIn('slow');
+  });   
+    
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+  });
+ 
+   $('.button_mini').each(function(i){
+    $(this).on('click', function() {
+      $('.order, .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+   });    
+
+//Валидация форм
+function validateForms(form){
+  $(form).validate({
+      rules: {
+          name: {
+              required: true,
+              minlength: 2
+          },
+          phone: "required",
+          email: {
+              required: true,
+              email: true
+          }
+      },
+      messages: {
+          name: {
+              required: "Пожалуйста, введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+      }
+  });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+  
+  // Вывод маски на номер телефона
+  $('input[name=phone]').mask("+38(999) 999-9999");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax ({
+      type:"POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+
+
+  });
+
+
+
+
+});
+})(jQuery);
